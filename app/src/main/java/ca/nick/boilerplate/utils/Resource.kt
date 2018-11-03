06416fdde73eeb5682838478c416a899c70e1d30
Nick Rose <nicholas.rose@cbc.ca>
2018-11-03 19:16:26 -0400

    Rename Resource.Failure -> Resource.Error

--- app/src/main/java/ca/nick/boilerplate/utils/Resource.kt
+++ app/src/main/java/ca/nick/boilerplate/utils/Resource.kt
@@ -3,5 +3,5 @@ package ca.nick.boilerplate.utils
 sealed class Resource<out T> {
     class Loading<out T> : Resource<T>()
     data class Success<out T>(val data: T) : Resource<T>()
-    data class Failure<out T>(val throwable: Throwable) : Resource<T>()
+    data class Error<out T>(val throwable: Throwable) : Resource<T>()
 }
