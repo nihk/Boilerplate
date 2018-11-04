abc9a6e59e579541794f8da8fc7be4efa7759eb2
Nick Rose <nicholas.rose@cbc.ca>
2018-11-03 22:29:53 -0400

    Add annotations to custom qualifier

--- app/src/main/java/ca/nick/boilerplate/di/annotation/StaleDataThreshold.kt
+++ app/src/main/java/ca/nick/boilerplate/di/annotation/StaleDataThreshold.kt
@@ -3,4 +3,6 @@ package ca.nick.boilerplate.di.annotation
 import javax.inject.Qualifier
 
 @Qualifier
+@MustBeDocumented
+@Retention(AnnotationRetention.RUNTIME)
 annotation class StaleDataThreshold
