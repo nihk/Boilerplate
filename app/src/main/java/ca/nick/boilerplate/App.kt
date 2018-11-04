e0ce87f5a494e0ad33491eca0597f9722c753d0a
Nick Rose <nicholas.rose@cbc.ca>
2018-11-03 21:50:33 -0400

    Add Timber dependency

--- app/src/main/java/ca/nick/boilerplate/App.kt
+++ app/src/main/java/ca/nick/boilerplate/App.kt
@@ -3,8 +3,17 @@ package ca.nick.boilerplate
 import ca.nick.boilerplate.di.components.DaggerAppComponent
 import dagger.android.AndroidInjector
 import dagger.android.support.DaggerApplication
+import timber.log.Timber
 
 class App : DaggerApplication() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
 
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> =
         DaggerAppComponent.builder()
